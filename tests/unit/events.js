c08a6e98f481cfaff6cabe97730e2b85e4a6532c
Steve Calvert <scalvert@mobify.com>
2015-04-22 15:40:40 -0700

    Step 5 code

--- tests/unit/events.js
+++ tests/unit/events.js
@@ -31,91 +31,96 @@ define([
             testSandbox.setUp('sandbox', setUpComplete);
         });
 
-        it('restricts length of input', function() {
-            $element.seesee();
+        describe('internal events respond correctly', function() {
+            it('restricts length of input', function() {
+                $element.seesee();
 
-            $element.val('4500 0000 0000 0000');
-            sendKey('0');
+                $element.val('4500 0000 0000 0000');
+                sendKey('0');
 
-            expect($element.val()).to.equal('4500 0000 0000 0000');
-        });
-
-        it('restricts type of input to numeric', function() {
-            $element.seesee();
-
-            $element.val('4500');
-            sendKey('f');
-
-            expect($element.val()).to.equal('4500');
-        });
-
-        it('allows numeric input', function() {
-            $element.seesee();
-
-            $element.val('4500');
-            sendKey('0');
-
-            expect($element.val()).to.equal('4500 0');
-        });
-
-        it('correctly formats card', function() {
-            $element.seesee();
-
-            $element.val('450000000000000');
-            sendKey('0');
-
-            expect($element.val()).to.equal('4500 0000 0000 0000');
-        });
-
-        it('correctly identifies visa card type and sets class', function() {
-            $element.seesee();
-
-            $element.val('450000000000000');
-            sendKey('0');
-
-            expect($element.hasClass('visa')).to.be.true;
-        });
-
-        it('correctly identifies mastercard card type and sets class', function() {
-            $element.seesee();
-
-            $element.val('540000000000000');
-            sendKey('0');
-
-            expect($element.hasClass('mastercard')).to.be.true;
-        });
-
-        it('correctly ignores unrecognized card types', function() {
-            $element.seesee();
-
-            $element.val('770000000000000');
-            sendKey('0');
-
-            expect($element.hasClass('visa')).to.be.false;
-            expect($element.hasClass('mastercard')).to.be.false;
-        });
-
-        it('correctly invokes the identified event', function(done) {
-            $element.seesee({
-                identified: function() {
-                    done();
-                }
+                expect($element.val()).to.equal('4500 0000 0000 0000');
             });
 
-            $element.val('450000000000000');
-            sendKey('0');
-        });
+            it('restricts type of input to numeric', function() {
+                $element.seesee();
 
-        it('passes data to the identified event', function(done) {
-            $element.seesee({
-                identified: function(e, ui) {
-                    expect(ui.type.type).to.equal('visa');
-                    done();
-                }
+                $element.val('4500');
+                sendKey('f');
+
+                expect($element.val()).to.equal('4500');
             });
 
-            $element.val('450000000000000');
-            sendKey('0');
+            it('allows numeric input', function() {
+                $element.seesee();
+
+                $element.val('4500');
+                sendKey('0');
+
+                expect($element.val()).to.equal('4500 0');
+            });
+
+            it('correctly formats card', function() {
+                $element.seesee();
+
+                $element.val('450000000000000');
+                sendKey('0');
+
+                expect($element.val()).to.equal('4500 0000 0000 0000');
+            });
+
+            it('correctly identifies visa card type and sets class', function() {
+                $element.seesee();
+
+                $element.val('450000000000000');
+                sendKey('0');
+
+                expect($element.hasClass('visa')).to.be.true;
+            });
+
+            it('correctly identifies mastercard card type and sets class', function() {
+                $element.seesee();
+
+                $element.val('540000000000000');
+                sendKey('0');
+
+                expect($element.hasClass('mastercard')).to.be.true;
+            });
+
+            it('correctly ignores unrecognized card types', function() {
+                $element.seesee();
+
+                $element.val('770000000000000');
+                sendKey('0');
+
+                expect($element.hasClass('visa')).to.be.false;
+                expect($element.hasClass('mastercard')).to.be.false;
+            });
         });
+        
+        describe('options events respond correctly', function() {
+            it('correctly invokes the identified event', function(done) {
+                $element.seesee({
+                    identified: function() {
+                        done();
+                    }
+                });
+
+                $element.val('450000000000000');
+                sendKey('0');
+            });
+
+            it('passes data to the identified event', function(done) {
+                $element.seesee({
+                    identified: function(e, ui) {
+                        expect(ui.type.type).to.equal('visa');
+                        done();
+                    }
+                });
+
+                $element.val('450000000000000');
+                sendKey('0');
+            });
+        });
+
     });
 });
