97215aba309b8b9f59c2c1f950d8b7298abcc7e4
Steve Calvert <scalvert@mobify.com>
2015-04-22 14:53:34 -0700

    Update to codez

--- dist/seesee.js
+++ dist/seesee.js
@@ -13,6 +13,35 @@
         SEESEE: 'seesee'
     };
 
+    /**
+     * Function.prototype.bind polyfill required for < iOS6
+     */
+    /* jshint ignore:start */
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== 'function') {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                            ? this
+                            : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
+    /* jshint ignore:end */
+
     function Seesee(element, options) {
         Seesee.__super__.call(this, element, options, Seesee.DEFAULTS);
     }
